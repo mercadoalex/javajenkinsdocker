3c3179098ce32723204a9712e1a7cbc5b229714e
Alex Market <mercadoalex@gmail.com>
2021-10-06 00:07:36 -0500

    Java jenkins project

---
+++ src/test/java/TestMain.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class TestMain {
+    @Test
+    public void testInputIsEven(){
+        assertTrue(Main.checkIfInputIsAnEvenNumber(22)); // Assertion
+    }
+}
